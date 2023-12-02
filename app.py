e9ba9c753c3fa62e7314b11fd4ef27ad98dea29b
mitchredd77 <147752683+mitchredd77@users.noreply.github.com>
2023-12-02 16:38:55 -0700

    Added edit function

--- app.py
+++ app.py
@@ -13,6 +13,12 @@ def clean_date(date):
     datetime_val = datetime.datetime(year, month, day)
     return datetime_val
 
+def clean_date_edit(datetime_val):
+    datetime_string = datetime_val
+    datetime_object = datetime.datetime.strptime(datetime_string, "%Y-%m-%d %H:%M:%S")
+    return datetime_object
+
+# converted_date = f"{month}/{day}/{year}"
 @app.route('/')
 def index():
     projects = Project.query.all()
@@ -20,6 +26,7 @@ def index():
 
 @app.route('/projects/new', methods=['GET', 'POST'])
 def add_project():
+    projects = Project.query.all()
     if request.form:
         new_project = Project(title=request.form['title'], date=request.form['date'],
                                description=request.form['desc'], skills=request.form['skills'],
@@ -28,16 +35,33 @@ def add_project():
         db.session.add(new_project)
         db.session.commit()
         return redirect(url_for('index'))     
-    return render_template('new.html')
+    return render_template('new.html', projects=projects)
 
-@app.route('/project/<id>')
+@app.route('/projects/<id>')
 def detail(id):
-    project = Project.query.get(id)
-    return render_template('detail.html', project=project)
+    projects = Project.query.all()
+    project_id = Project.query.get(id)
+    return render_template('detail.html', projects=projects, project_id=project_id)
+
+@app.route('/projects/<id>/edit', methods=['GET', 'POST'])
+def edit_project(id):
+    projects = Project.query.all()
+    project_id = Project.query.get(id)
+    if request.form:
+        project_id.title = request.form['title']
+        project_id.date = request.form['date']
+        project_id.date = clean_date_edit(project_id.date)
+        project_id.description = request.form['desc']
+        project_id.skills = request.form['skills']
+        project_id.link = request.form['github']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('editproject.html', project_id=project_id, projects=projects)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    projects = Project.query.all()
+    return render_template('about.html', projects=projects)
 
 
 
